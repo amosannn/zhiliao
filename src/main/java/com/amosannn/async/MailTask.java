1b75057b927516735aabc75bc14cc09542ad79b1
amosannn <amosannn@gmail.com>
2018-04-10 22:03:23 +0800

    新增账号激活功能

--- src/main/java/com/amosannn/async/MailTask.java
+++ src/main/java/com/amosannn/async/MailTask.java
@@ -63,13 +63,13 @@ public class MailTask implements Runnable{
         sb.append("<html><head></head><body>");
 
         if (operation == 1) {
-          sb.append("<a href=" + "http://127.0.0.1:8080/" + "activate?code=");
+          sb.append("<a href=" + "http://127.0.0.1:8080/zhiliao/" + "activate?code=");
           sb.append(code);
           sb.append(">点击激活</a></body>");
         } else {
           sb.append("是否将您的密码修改为:");
           sb.append(code.substring(0, 8));
-          sb.append("，<a href=" + "http://127.0.0.1:8080/" + "verify.do?code=" + code + ">");
+          sb.append("，<a href=" + "http://127.0.0.1:8080/zhiliao/" + "verify?code=" + code + ">");
           sb.append("点击是</a></body>");
         }
 
